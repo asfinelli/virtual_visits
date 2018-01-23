c19efe71fe2135d08fbe7b4e6a1e1faa75ec9c19
Drew Finelli <asfinelli@gmail.com>
2018-01-23 08:58:50 -0500

    updated routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-# before_action :require_logged_in, :require_doctor_logged_in, 
   def index
     @appointments = Appointment.where(user: current_user)
   end
